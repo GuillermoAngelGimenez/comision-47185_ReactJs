cbcc4a2b869c02d337a2c916b4171c6519fdb1c0
guillermo gimenez <angel_gag@hotmail.com>
2023-08-31 07:47:20 -0300

    creacion de archivos

---
+++ preentrega1-gimenez/src/App.js
@@ -0,0 +1,12 @@
+import Saludo from "./Components/Saludo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Saludo />
+    </div>
+  );
+}
+
+export default App;
